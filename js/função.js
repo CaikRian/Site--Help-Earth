7d224bf71ed33ad8db99e43d68fa9345628ed5db
Caik Rian <caik.job1707@gmail.com>
2022-07-30 21:19:08 -0300

    Site - Help Earth

---
+++ js/função.js
@@ -0,0 +1,89 @@
+
+
+function img01 (){
+    
+    document.getElementById('imgFundo').style.backgroundImage = 'url("img/desmatamento.jpg")';
+    document.getElementById("titulo").textContent="Desmatamento";
+    document.getElementById("par1").textContent="Também chamado de desflorestamento, consiste na retirada da cobertura vegetal parcial ou total de um determinado lugar. Enquanto alguns enxergam essa prática como uma ação necessária ao suprimento das necessidades do ser humano, outros apontam o desmatamento como um dos maiores problemas ambientais da atualidade.";
+    document.getElementById("par2").textContent="A questão do desmatamento tomou grandes proporções a partir da Revolução Industrial. A introdução de novas tecnologias (que proporcionaram o aumento da produção industrial) e o consumo (que aumentou consideravelmente) fizeram com que diversas florestas temperadas e tropicais fossem devastadas, a fim de atender a essa nova demanda.";
+    document.getElementById("par3").textContent="As principais consequências estão relacionadas ao meio ambiente e a tudo que lhe diz respeito. Ao desmatar, compromete-se toda a biodiversidade da área. Espécies da fauna perdem seu habitat e espécies da flora podem entrar para a lista de ameaças à extinção e assim causar um enorme desequilíbrio ambiental, prejudicando até mesmo as atividades primárias, das quais dependem muitas famílias, e também a economia, como a caça, a agricultura e a pecuária.";
+
+    
+}
+function img02 (){
+    
+    document.getElementById('imgFundo').style.backgroundImage = 'url("img/MudancaClimatica.jpg")';
+    document.getElementById("titulo").textContent="Mudanças Climáticas";
+    document.getElementById("par1").textContent="Mudanças climáticas são alterações provocadas nos padrões climáticos a longo prazo com base nas alternâncias meteorológicas, ou seja, nas condições do tempo observadas por um período.";
+    document.getElementById("par2").textContent="A partir da Revolução Industrial, toneladas de gases de efeito estufa, especialmente o dióxido de carbono, foram lançadas à atmosfera. Sabe-se que a atmosfera terrestre é formada por gases como oxigênio, nitrogênio, dióxido de carbono e também vapor d'água. Esses gases, principalmente o gás carbônico, possuem a capacidade de absorver a radiação solar emitida à superfície terrestre.";
+    document.getElementById("par3").textContent="As mudanças climáticas geram uma série de consequências ambientais graves, muitas até já podem ser observadas atualmente. Uma das consequências do aumento da temperatura do planeta é o aumento do nível do mar, que ocorre em virtude do degelo das geleiras. Isso pode resultar na inundação e na submersão de áreas costeiras, causando diversos prejuízos às pessoas que vivem nessas áreas.";
+
+}
+function img03 (){
+    
+    document.getElementById('imgFundo').style.backgroundImage = 'url("img/PoluicaoDoAr.jpg")';
+    document.getElementById("titulo").textContent="Poluição do Ar";
+    document.getElementById("par1").textContent="A poluição atmosférica é consequência, em maior parte, da ação humana, no sentido de introduzir produtos químicos e/ou tóxicos no ambiente.";
+    document.getElementById("par2").textContent="A queima de combustíveis fósseis – e não só ela - propicia a liberação de monóxido de carbono, que corresponde a aproximadamente 45% dos poluentes liberados em grandes metrópoles. Inodoro e incolor, o CO tem capacidade de se ligar à hemoglobina sanguínea, podendo provocar asfixia.";
+    document.getElementById("par3").textContent="Dióxido de nitrogênio, dióxido de enxofre, ácido nítrico, ácido sulfúrico e hidrocarbonetos são outros poluentes que contribuem para esse tipo de poluição. Irritação de mucosas e vias respiratórias, cânceres, alteração da água e solo, corrosões de construções e monumentos, inversão térmica, efeito de estufa e destruição da camada de ozônio são algumas consequências da ação desses. Partículas, como as de sílica e amianto podem ser cancerígenas, além de causar fibroses e enfisemas pulmonares.";
+
+}
+function img04 (){
+    
+    document.getElementById('imgFundo').style.backgroundImage = 'url("img/PoluicaoDaAgua.jpg")';
+    document.getElementById("titulo").textContent="Poluição da Água";
+    document.getElementById("par1").textContent="A água que pode ser consumida sem risco para a saúde está extinta na Terra. Isso se deve ao fato que, com o passar dos anos, esse bem tão importante para toda espécie de vida, vem sendo ameaçada pela poluição.";
+    document.getElementById("par2").textContent="A grande concentração humana nas cidades também é responsável por uma parte importante da contaminação. Ali surgem verdadeiros rios de esgoto que arrastam resíduos sólidos. E lembrem-se, à primeira vista as águas podem se apresentar puras, mas nem todas são potáveis. Por isso é importante que tenha conhecimento da água que você está consumindo, tome cuidado com águas de cisternas e minas. É indispensável que ela passe por um tratamento prévio.";
+    document.getElementById("par3").textContent=" Aágua tóxica não pode ser bebida nem usada em atividades essenciais como a agricultura. Além disso, é uma fonte de insalubridade que provoca mais de 500.000 mortes anuais a nível global por diarreia e transmite doenças como cólera, disenteria, febre tifoide e poliomielite.";
+
+}
+function img05 (){
+    
+    document.getElementById('imgFundo').style.backgroundImage = 'url("img/DegradaçãoDoSolo.jpg")';
+    document.getElementById("titulo").textContent="Degradação do Solo";
+    document.getElementById("par1").textContent="O solo é um dos elementos naturais mais importantes, uma vez que todos os seres vivos estão sobre ele. Todo sustento humano é retirado desse recurso. Apesar da extrema importância que o solo desempenha, o seu uso realizado de forma inadequada facilita o aparecimento de vários problemas.";
+    document.getElementById("par2").textContent="As atividades agrícolas são agentes degradantes dos solos, causando anualmente a perda de milhões de toneladas.";
+    document.getElementById("par3").textContent="O crescente aumento das erosões é resultado da forma equivocada de plantio desenvolvida por muitos agricultores ao longo do território brasileiro, isso tem transformado grandes áreas produtivas em solos inférteis";
+
+}
+function img06 (){
+    
+    document.getElementById('imgFundo').style.backgroundImage = 'url("img/GeracaoDeResiduos.jpg")';
+    document.getElementById("titulo").textContent="Geração de Resíduos";
+    document.getElementById("par1").textContent="O aumento do número de habitantes no planeta, associado à concentração das populações nas cidades, vem agravando a capacidade natural da terra para absorver o lixo.";
+    document.getElementById("par2").textContent="Os principais causadores desse tipo de poluição são os efluentes industriais (produtos químicos, metais pesados), agrícolas (fertilizantes outros tipos de agrotóxico), o esgoto doméstico e o chorume oriundo da decomposição de resíduos.";
+    document.getElementById("par3").textContent="Os impactos da má gestão dos resíduos sólidos causam poluição atmosférica, poluição hídrica, poluição do solo e poluição visual, e, além disso, dependendo do tipo de resíduos, podem causar doenças para população, ocasionando o dano a saúde das pessoas.";
+
+}
+function img07 (){
+    
+    document.getElementById('imgFundo').style.backgroundImage = 'url("img/ExtincaoDeEspecie.jpg")';
+    document.getElementById("titulo").textContent="Extinção de Espécies";
+    document.getElementById("par1").textContent="Animais ameaçados de extinção são aqueles que correm risco de desaparecer de maneira definitiva do nosso planeta, ou seja, de tornarem-se extintos. Apesar de ser relativamente comum na natureza, atualmente, o processo de extinção tem sido intensificado pelo homem.";
+    document.getElementById("par2").textContent="A extinção pode ocorrer por vários motivos, dentre eles, destacam-se a destruição do habitat, competição, doenças, caça e matanças deliberadas, mudanças ambientais drásticas e catástrofes ambientais.";
+    document.getElementById("par3").textContent="As principais consequências da extinção de espécies são a perda de biodiversidade, a redução do fundo genético global do planeta, a diminuição do número de recursos naturais e de variedade alimentar, a redução da capacidade de autorregulação dos ecossistemas e a aceleração da extinção de outras espécies.";
+
+}
+function img08 (){
+    
+    document.getElementById('imgFundo').style.backgroundImage = 'url("img/ModificacaoGenetica.jpg")';
+    document.getElementById("titulo").textContent="Modificação Genética";
+    document.getElementById("par1").textContent="São chamados de alimentos transgênicos aqueles que tiveram seu material genético modificado. Essa modificação é feita pela inserção de um gene de outro organismo. Também são considerados transgênicos alimentos que possuem em sua composição algum ingrediente ou matéria-prima que passou pelo processo de modificação.";
+    document.getElementById("par2").textContent="A modificação genética de certos alimentos é feita com o objetivo de aumentar a sua resistência contra doenças causadas por insetos ou fungos e diminuir a necessidade do uso de agrotóxicos.";
+    document.getElementById("par3").textContent="Quando se insere um gene de um ser em outro, novos compostos podem ser formados nesse organismo, como proteínas e aminoácidos. Se este organismo modificado geneticamente for um alimento, seu consumo pode provocar alergias em parcelas significativas da população, por causa dessas novas substâncias.";
+
+}
+
+
+function RolarEsquerda(x, y) {
+    Materias.scrollBy(x, y);
+    
+    
+}
+function RolarDireita(x, y) {
+    Materias.scrollBy(x, y);
+    
+}
+
+
+
